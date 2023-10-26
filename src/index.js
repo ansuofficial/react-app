b6a1ebc582af40b8cca56d849804c967bf2a1d8b
Ansuofficial <badjieansu165@gmail.com>
2023-10-26 10:55:08 +0000

    react-app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./Navbar.js";
-import "./App.css";
+// import "./App.css";
 
-const firstBook = {
-  title: "Interesting Facts For Curious Minds",
-  authur: "Jordan Moore",
-  img: "https://m.media-amazon.com/images/I/71hwUY5ZmxL._SY342_.jpg",
-  num: 20,
+const books = [
+  {
+    title: "Interesting Facts For Curious Minds",
+    authur: "Jordan Moore",
+    img: "https://m.media-amazon.com/images/I/71hwUY5ZmxL._SY342_.jpg",
+    num: 20,
+  },
+
+  {
+    title: "Killers of the Flower Moon",
+    authur: " David Grann",
+    img: "https://m.media-amazon.com/images/I/81Xy1ugiWeL._SY342_.jpg",
+    num: 10,
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="bookList">
+      {books.map((b) => {
+        const { img, title, authur, children } = b;
+        return (
+        <Book img={img} title={title} authur={authur} />
+        )
+      })}
+    </section>
+  );
 };
 
-const secondBook = {
-  title: "Killers of the Flower Moon",
-  authur: " David Grann",
-  img: "https://m.media-amazon.com/images/I/81Xy1ugiWeL._SY342_.jpg",
-  num: 10,
-};
-
-const Book = ({img, title, authur, children}) => {
+const Book = (props) => {
+  const {img, title, authur, children} = props
   return (
     <article className="book">
       <img src={img} alt={title} />
@@ -28,28 +44,26 @@ const Book = ({img, title, authur, children}) => {
   );
 };
 
-const BookList = () => {
-  return (
-    <section>
-      <Navbar />
-      <div className="bookList">
-        <Book
-          authur={firstBook.authur}
-          title={firstBook.title}
-          img={firstBook.img}
-        >
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam fugiat ad saepe!</p>
-          <button>Click me</button>
-        </Book>
-        <Book
-          authur={secondBook.authur}
-          title={secondBook.title}
-          img={secondBook.img}
-        />
-      </div>
-    </section>
+// const BookList = () => {
+//   return (
+//     <section>
+//       <Navbar />
+//       <div className="bookList">
+//         <Book
+//           authur={firstBook.authur}
+//           title={firstBook.title}
+//           img={firstBook.img}
+//        />
 
-  );
-};
+//         <Book
+//           authur={secondBook.authur}
+//           title={secondBook.title}
+//           img={secondBook.img}
+//         ></Book>
+//       </div>
+//     </section>
+
+//   );
+// };
 
 ReactDOM.createRoot(document.getElementById("root")).render(<BookList />);
